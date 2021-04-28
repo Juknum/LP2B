ee33a6ccbec9d8b71f3c0fea272c71ed040e38fd
Julien <49886317+Juknum@users.noreply.github.com>
2021-04-28 13:22:53 +0200

    add files from TD1

---
+++ TD1/Assets/Scripts/BasketControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketControl : MonoBehaviour {
+
+    protected int score = 0;
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(1.5f * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(1.5f * Time.deltaTime, 0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) {
+        score++;
+        Debug.Log("Current score:" + score);
+    }
+}
